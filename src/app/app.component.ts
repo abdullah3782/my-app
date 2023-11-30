a371d4ac2e9c52621460e2ca8724d78e04af5535
abdullah3782 <abdullah.butt093@gmail.com>
2023-11-30 11:30:13 +0100

    sa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Component1Component } from './components/component1/component1.component';
+import { Component2Component } from "./components/component2/component2.component";
+
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, Component1Component]
+    imports: [CommonModule, RouterOutlet, Component1Component, Component2Component]
 })
 export class AppComponent {
   title = 'my-app';
--- src/app/components/component1/component1.component.ts
+++ src/app/components/component1/component1.component.ts
@@ -1,10 +1,11 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-component1',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './component1.component.html',
   styleUrl: './component1.component.css'
 })
@@ -13,4 +14,37 @@ export class Component1Component {
 //Atributos de la clase
 customer = "Mar";
 age: number = 21;
+years:any=["12,13,12,12"];
+nom: string = "Mav";
+valor : number =1;
+frutas : any [] = [
+
+{
+nombre:"pera",
+precio:1.80,
+cantidad:4
+},
+
+{
+  nombre:"manzana",
+  precio:1.20,
+  cantidad:12
+  },
+  {
+    nombre:"naranja",
+    precio:2.80,
+    cantidad:15
+    }
+
+]
+//metodos de la clase
+subir() {
+this.valor++;
+}
+
+borrar(nombreFruta:string) {
+
+this.frutas=this.frutas.filter(x=>x.nombre !=nombreFruta);
+
+}
 }
